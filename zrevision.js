cb42b31936133713cdf045b8b959108c86b35f4d
vinay <abc@gmail.com>
2026-06-06 17:01:05 +0530

    zerodha backend

---
+++ zrevision.js
@@ -0,0 +1,35 @@
+// exploring the  dash board ko phir se dekhna hai 
+
+// FREE CODE CAMP SITE JHA AAP MERN KA USE KAISE BANAY JO BHE PTA KAR SAKTE HAI JYADA JANNNE 
+// KE LIYE CHATGTP PAR PASTE KARE FREE CODE CAPM KYA HAI BATATO YE MERN KO KAISE HELP KARTA HAI 
+
+// chart .js pakage ya libarray for ploting graph 
+
+// export const data = {
+//   labels,
+//   datasets: [
+//     {
+//       label: 'Dataset 1',
+//       data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+//       backgroundColor: 'rgba(255, 99, 132, 0.5)',
+//     },
+//     {
+//       label: 'Dataset 2',
+//       data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+//       backgroundColor: 'rgba(53, 162, 235, 0.5)',
+//     },
+//   ],
+// };
+
+//FOR THE LIVE API DATA YOU HAVE TO PAY FOR IT FOR NOW I WILL TELL WHICH KIND OF API ARE EXIST FOR THE EXACT DATA OF STOK
+//MARKET 
+// 1 - UPSTOX
+//2  - SSI AND MANY MORE 
+
+                             //      TESTING RULE                             //
+
+
+  //                  1- UNIT-TESTING => JAB PARALLEL DEVLOPER KAM KARTE ALAG ALAG COMPONENT ME TO HAM MAKE SURE KARNA HOTA HAI KE SABHI KA CODE PROPER CHAL RHA HO OR CODING STANDERD KE RULS KO FOLLOW KAR RHA HO  
+   //                                      ISKO HUM MANULLY TESTING BOLTE HAI JISME IK DEVLOPR SARE COMPONNET KO IK IK KAR KE DEHEGA OR PTA LAGAYEGA KE JO KHA GYA TH KE IS BUUTTON KO CLICK KARNE PAR YE HONA CHHAHIYE VO HO RH AHI HAI KE NHI        
+                    //   2-ATOMATION YA SRCIPT TESING => BAR BAR TEST NA  PADE YADI FORTHER KUCH HOTA HAI APPLICATION ME YE KAREGA SCRIPT LIKHNE PAR ISKO JEST PAKAGE KE SATH KARNA HOTA HAI  FOR MORE JESTJS.IO 
+
