cca8d73ce499be9265c57b9f95049d81ec3633b5
vinay <abc@gmail.com>
2026-06-12 23:31:13 +0530

    updated

--- Routes/AuthRoute.js
+++ Routes/AuthRoute.js
@@ -6,4 +6,9 @@ router.post('/signup', Signup)
 router.post('/login', Login)
 router.post('/verify',userVerification)
 
+router.post('/logout',(req,res)=>{
+    res.cookie("token","",{expires:new Date(0)});
+    res.json({success:true});
+})
+
 module.exports = router
