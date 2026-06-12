28e2dabfca896f056249890be6a2a566718f7050
vinay <abc@gmail.com>
2026-06-12 21:03:56 +0530

    updated

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -17,17 +17,11 @@ module.exports.Signup = async (req, res, next) => {
 
     res.cookie("token", token, {
       withCredentials: true,
-      httpOnly: false,
+      // httpOnly: false,
+    httpOnly: true,
       sameSite: "none",
       secure: true,
     });
-
-
-
-
-
-
-
 
 
     res
@@ -62,7 +56,7 @@ module.exports.Login = async (req, res, next) => {
 
     res.cookie("token", token, {
       withCredentials: true,
-      httpOnly: false,
+      httpOnly: true,
       sameSite: "none",
       secure: true,
     });
@@ -77,3 +71,103 @@ module.exports.Login = async (req, res, next) => {
     console.error(error);
   }
 }
+
+
+
+// const User = require("../model/user");
+// const bcrypt = require("bcryptjs");
+// const { createSecretToken } = require("../utils/SecretToken");
+
+// // SIGNUP
+// module.exports.Signup = async (req, res) => {
+//   const { name, email, password } = req.body;
+
+//   const userExists = await User.findOne({ email });
+//   if (userExists) return res.json({ success: false, message: "User exists" });
+
+//   const hashed = await bcrypt.hash(password, 10);
+
+//   const user = await User.create({
+//     name,
+//     email,
+//     password: hashed,
+//   });
+
+//   const token = createSecretToken(user._id);
+
+//   res.cookie("token", token, {
+//     httpOnly: true,
+//     sameSite: "lax",
+//   });
+
+//   res.json({ success: true, message: "Signup success" });
+// };
+
+// // LOGIN
+// module.exports.Login = async (req, res) => {
+//   const { email, password } = req.body;
+
+//   const user = await User.findOne({ email });
+//   if (!user) return res.json({ success: false });
+
+//   const match = await bcrypt.compare(password, user.password);
+//   if (!match) return res.json({ success: false });
+
+//   const token = createSecretToken(user._id);
+
+//   res.cookie("token", token, {
+//     httpOnly: true,
+//     sameSite: "lax",
+//   });
+
+//   res.json({ success: true });
+// };
+
+// // LOGOUT
+// module.exports.Logout = async (req, res) => {
+//   res.cookie("token", "", { expires: new Date(0) });
+//   res.json({ success: true });
+// };
+
+
+
+
+
+
+
+// // authMidleware
+
+// const jwt = require("jsonwebtoken");
+// const User = require("../model/user");
+
+// module.exports.userVerification = async (req, res) => {
+//   try {
+//     const token = req.cookies.token;
+
+//     // ❌ no token
+//     if (!token) {
+//       return res.json({ status: false });
+//     }
+
+//     // verify token
+//     jwt.verify(token, process.env.TOKEN_KEY, async (err, data) => {
+//       if (err) {
+//         return res.json({ status: false });
+//       }
+
+//       // find user from token id
+//       const user = await User.findById(data.id);
+
+//       if (user) {
+//         return res.json({
+//           status: true,
+//           user: user.name,
+//         });
+//       }
+
+//       return res.json({ status: false });
+//     });
+//   } catch (err) {
+//     return res.json({ status: false });
+//   }
+// };
--- Middlewares/AuthMiddleware.js
+++ Middlewares/AuthMiddleware.js
@@ -17,3 +17,5 @@ module.exports.userVerification = (req, res) => {
     }
   })
 }
+
+
